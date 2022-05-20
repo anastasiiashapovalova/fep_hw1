988c4fe068db7e9d2432c560f039fd5e8da9c8bb
anastasiiashapovalova <anastasiiaoxk16@gmail.com>
2022-05-20 11:23:20 +0200

    corrected

--- hw4.0.0/main_1.js
+++ hw4.0.0/main_1.js
@@ -1,11 +1,14 @@
 function enterNumber () {
     let value = prompt("Enter a number greater than 100");
-    for (let i = 0; i < 9; i++) {
+    for (let i = 0; i <= 9; i++) {
         if (value < 100) {
             value = prompt("Try again");
-        } else {
-            break;
+        }
+        if (typeof value === "string") {
         }
     }
     return console.log(value);
 }
+
+
+
